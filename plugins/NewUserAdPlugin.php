fcdfb96ec534d7683779f8382e55c60ad6347acf
Michael Shepard <joelisester@gmail.com>
2013-06-29 21:43:35 -0400

    Fixed Red's plugin a bit.

--- plugins/NewUserAdPlugin.php
+++ plugins/NewUserAdPlugin.php
@@ -11,15 +11,16 @@ class NewUserAdPlugin extends Plugin {
 		if(!common_logged_in() && $action instanceof PublicAction) {
 			// Show a little info pane to not-logged-in users on Public to convince them to join
 			$action->elementStart('div', array('id' => 'please_join_our_site'));
-			$action->raw(<<<PORK
+            $register = common_local_url('register');
+			$action->raw(<<<HTML
 <p>Welcome to <strong>Rainbow Dash Network</strong>, a social networking site for bronies! Whether you want a place to share your creations or just a spot to make some new friends, RDN is the place to be!</p>
 <ul>
 <li id="get_to_know_us">Get to know your fellow ponies! Share some art you created, talk about episodes, or just chat it up. We even have a synchronized room to watch episodes together!</li>
 <li id="join_some_groups">Join groups for people with similar interests, or create your own and start a whole new community! Meetup groups, game clans, and more are all welcome.</li>
 <li id="so_many_posts">Post polls, events, and more! We support a variety of posts to suit your every need.</li>
 </ul>
-<p>Feel free to browse the site to learn more about our community. If you like what you see, <a href="/main/register">register an account and start posting!</a> We're always welcoming to new faces!</p>
-PORK
+<p>Feel free to browse the site to learn more about our community. If you like what you see, <a href="$register">register an account and start posting!</a> We're always welcoming to new faces!</p>
+HTML
 );
 			$action->elementEnd('div');
 		}
