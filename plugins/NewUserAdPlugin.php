dc5c74436f1ef7678981a6654927f5b237759327
RedEnchilada <redenchilada@derpymail.com>
2014-04-18 12:13:04 -0500

    Change text in new user blurb to stop advertising features that don't exist

--- plugins/NewUserAdPlugin.php
+++ plugins/NewUserAdPlugin.php
@@ -15,9 +15,9 @@ class NewUserAdPlugin extends Plugin {
 			$action->raw(<<<HTML
 <p>Welcome to <strong>Rainbow Dash Network</strong>, a social networking site for bronies! Whether you want a place to share your creations or just a spot to make some new friends, RDN is the place to be!</p>
 <ul>
-<li id="get_to_know_us">Get to know your fellow ponies! Share something you made, talk about episodes, or just chat it up. We even have a synchronized room to watch episodes together!</li>
+<li id="get_to_know_us">Get to know your fellow ponies! Share something you made, talk about episodes, or just chat it up. We even have a synchronized video player!</li>
 <li id="join_some_groups">Join groups for people with similar interests, or create your own and start a whole new community! Meetup groups, game clans, and more are all welcome.</li>
-<li id="so_many_posts">Post polls, events, and more! We support a variety of posts to suit your every need.</li>
+<li id="so_many_posts">Our software is open-source and free to use or contribute to. We're always growing and developing new ways to improve the site experience.</li>
 </ul>
 <p>Feel free to browse the site to learn more about our community. If you like what you see, <a href="$register">register an account and start posting!</a> We're always welcoming to new faces!</p>
 HTML
