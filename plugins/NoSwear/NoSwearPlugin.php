604bacfa48d99a62a6de79b1a0afa980e138f929
RedEnchilada <redenchilada@derpymail.com>
2014-05-27 01:13:39 -0500

    Remove discrimination against Applejack's face

--- plugins/NoSwear/NoSwearPlugin.php
+++ plugins/NoSwear/NoSwearPlugin.php
@@ -28,7 +28,7 @@ d[i$s][c$s]?khead
 c[u$s]nt
 s[h$s][i$s]t(?!su|a|o)e?(head)?
 vag(ina)?[sl]?\b
-(m[ou]th(er|uh|a))?f[r$s]?[uoe$s][c$s]?k(?!ushima)
+(m[ou]th(er|uh|a))?f[uoe$s][c$s]?k(?!ushima)
 \bf[r$s]?e[c$s]?k\b
 (da)?fuq
 b[ie$s]a?[t$s][c$s][h$s]
